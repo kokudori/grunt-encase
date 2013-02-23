775aa9355cbeab47389f46245bdbdeb55240174b
Samuel Selvanathan <sjasel@gmail.com>
2013-02-23 10:56:38 -0800

    added support to pass variables into the encased anonymous function

--- test/dest/params-browser-test.js
+++ test/dest/params-browser-test.js
@@ -1,9 +1,7 @@
-(function(undefined) {
+(function(w,d,undefined) {
 var hoge = 100,
 	piyo = 200,
 	foo = 300,
 	bar = 400;
 window.hoge = hoge
-window.piyo = piyo
-window.foo = foo
-})();
+})(window,document);
